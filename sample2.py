bcbeb768af3c217ccfea807c079efaf4d4f57dd1
takashi <tk19490612@gmail.com>
2020-05-03 12:49:42 +0900

    追加がしやすいコードを書いた 複数のパラメーターを使った

--- sample2.py
+++ sample2.py
@@ -1,3 +1,5 @@
+from symbol import parameters
+
 from sample1 import warikan
 
 
@@ -7,10 +9,44 @@ from sample1 import warikan
 def check_warikan():
     result = warikan(1500, 3) == "1人あたり: 500円, 端数: 0円"
 
-    if result == True:
+    if result:
         print("OK")
     else:
         print("NG")
+
+
+check_warikan()
+
+
+def check_warikan2():
+    result = warikan(2000, 3) == "1人あたり: 666円, 端数: 2円"
+
+    if result:
+        print("OK")
+    else:
+        print("NG")
+
+
+check_warikan2()
+
+
+def check_warikan():
+    parameters = [
+        (1500, 3, "1人あたり: 500円, 端数: 0円"),
+        (2000, 3, "1人あたり: 666円, 端数: 2円"),
+    ]
+
+    for p in parameters:
+        amount = p[0]
+        number_of_people = p[1]
+        expected = p[2]
+
+        result = expected == warikan(amount, number_of_people)
+
+        if result:
+            print("OK!")
+        else:
+            print("NG!")
 
 
 check_warikan()
