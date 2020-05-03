2c7b00da1d2c8d497b99a06738b6e86497ae8f6d
takashi <tk19490612@gmail.com>
2020-05-03 13:05:38 +0900

    if __name__ == "__main__":  print(warikan())  でsample2のTrueとか消える

--- sample1.py
+++ sample1.py
@@ -5,6 +5,5 @@ def warikan(amount, number_of_people):
     return f"1人あたり: {quotient}円, 端数: {remainber}円"
 
 
-check_result = warikan(1500, 3) == "1人あたり: 500円, 端数: 0円"
-print(check_result)
-
+if __name__ == "__main__":
+    print(warikan(1500, 3) == "1人あたり: 500円, 端数: 0円")
