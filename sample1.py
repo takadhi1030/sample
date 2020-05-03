1d5f766a56361db9e24bf525c284089a07a9d012
takashi <tk19490612@gmail.com>
2020-05-03 12:05:51 +0900

    リファクタリング

--- sample1.py
+++ sample1.py
@@ -1,10 +1,10 @@
-def calculate_warikan(amount, number_of_people):
+def warikan(amount, number_of_people):
     quotient = amount // number_of_people
     remainber = amount % number_of_people
 
     return f"1人あたり: {quotient}円,端数:{remainber}円"
 
 
-check_result = calculate_warikan(1500, 3) == "1人あたり: 500円, 端数: 0円"
+check_result = warikan(1500, 3) == "1人あたり: 500円, 端数: 0円"
 print(check_result)
 
