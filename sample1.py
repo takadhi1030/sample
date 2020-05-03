f1bed526bdd3f58a0a7e92f8272c7f7c0103cbbe
takashi <tk19490612@gmail.com>
2020-05-03 12:20:44 +0900

    どこが間違ってるかはわからない

--- sample1.py
+++ sample1.py
@@ -2,7 +2,7 @@ def warikan(amount, number_of_people):
     quotient = amount // number_of_people
     remainber = amount % number_of_people
 
-    return f"1人あたり: {quotient}円,端数:{remainber}円"
+    return f"1人あたり: {quotient}円, 端数: {remainber}円"
 
 
 check_result = warikan(1500, 3) == "1人あたり: 500円, 端数: 0円"
--- sample2.py
+++ sample2.py
@@ -4,10 +4,13 @@ from sample1 import warikan
 # 目標:calculate_warikanの挙動が正しいかどうかを調べるコードを書く
 # 入力と出力のパターン
 # 例1:1500円で3人 -> "1人あたり: 500円, 端数: 0円"
-def check_calculate_warikan():
-    warikan(1500, 3) == "1人あたり: 500円, 端数: 0円"
+def check_warikan():
+    result = warikan(1500, 3) == "1人あたり: 500円, 端数: 0円"
 
     if result == True:
         print("OK")
     else:
         print("NG")
+
+
+check_warikan()
